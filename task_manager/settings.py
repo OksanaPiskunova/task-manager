ea9dfbd64748a39671399e3f0bc837c7fc755657
OksanaPiskunova <oksana.piskunova.ok@gmail.com>
2017-02-24 16:55:11 +0300

    Added Employee model

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'task_manager',
+    'task_manager.task_manager',
 ]
 
 MIDDLEWARE = [
--- task_manager/task_manager/models.py
+++ task_manager/task_manager/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Employee(models.Model):
+    MANAGER = 'Manager'
+    DEVELOPER = 'Developer'
+    ROLES = (
+        (MANAGER, MANAGER),
+        (DEVELOPER, DEVELOPER),
+    )
+
+    user = models.OneToOneField(User)
+    role = models.CharField(
+        max_length=10,
+        choices=ROLES,
+        default=DEVELOPER
+    )
+
+
+@receiver(post_save, sender=User)
+def create_employee(sender, instance, created, **kwargs):
+    if created:
+        Employee.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_employee(sender, instance, **kwargs):
+    instance.employee.save()
