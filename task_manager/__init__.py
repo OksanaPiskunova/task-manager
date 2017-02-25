f8c03adca3d50d0b75d2f75009085d8a865fa67c
OksanaPiskunova <oksana.piskunova.ok@gmail.com>
2017-02-25 13:04:53 +0300

    Added Celery to the project

---
+++ task_manager/__init__.py
@@ -0,0 +1,2 @@
+from __future__ import absolute_import
+from task_manager.celery import app as celery_app
--- task_manager/settings.py
+++ task_manager/settings.py
@@ -102,6 +102,17 @@ REST_FRAMEWORK = {
 }
 
 
+# Celery
+
+BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Europe/Minsk'
+
+
+
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
